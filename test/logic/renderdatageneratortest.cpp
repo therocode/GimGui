ce15b7ef48296cc2b9f7dda85f385e1288332b82
Tobias Widlund <widlundtobias@gmail.com>
2015-03-10 16:33:44 +0100

    Made funtcions for testing quads

--- test/logic/renderdatageneratortest.cpp
+++ test/logic/renderdatageneratortest.cpp
@@ -7,24 +7,62 @@
 
 void checkQuadPositions(const float* firstPosition, float xMin, float yMin, float xMax, float yMax)
 {
-                CHECK(closeEnough(*(firstPosition + 0) ,  xMin));//x
-                CHECK(closeEnough(*(firstPosition + 1) ,  yMin));//y
-                CHECK(closeEnough(*(firstPosition + 2) ,  0.0f));
-                CHECK(closeEnough(*(firstPosition + 3) ,  xMin));//x
-                CHECK(closeEnough(*(firstPosition + 4) ,  yMax));//y
-                CHECK(closeEnough(*(firstPosition + 5) ,  0.0f));
-                CHECK(closeEnough(*(firstPosition + 6) ,  xMax));//x
-                CHECK(closeEnough(*(firstPosition + 7) ,  yMax));//y
-                CHECK(closeEnough(*(firstPosition + 8) ,  0.0f));
-                CHECK(closeEnough(*(firstPosition + 9) ,  xMax));//x
-                CHECK(closeEnough(*(firstPosition + 10),  yMax));//y
-                CHECK(closeEnough(*(firstPosition + 11),  0.0f));
-                CHECK(closeEnough(*(firstPosition + 12),  xMax));//x
-                CHECK(closeEnough(*(firstPosition + 13),  yMin));//y
-                CHECK(closeEnough(*(firstPosition + 14),  0.0f));
-                CHECK(closeEnough(*(firstPosition + 15),  xMin));//x
-                CHECK(closeEnough(*(firstPosition + 16),  yMin));//y
-                CHECK(closeEnough(*(firstPosition + 17),  0.0f));
+    CHECK(closeEnough(*(firstPosition + 0) ,  xMin));//x
+    CHECK(closeEnough(*(firstPosition + 1) ,  yMin));//y
+    CHECK(closeEnough(*(firstPosition + 2) ,  0.0f));
+    CHECK(closeEnough(*(firstPosition + 3) ,  xMin));//x
+    CHECK(closeEnough(*(firstPosition + 4) ,  yMax));//y
+    CHECK(closeEnough(*(firstPosition + 5) ,  0.0f));
+    CHECK(closeEnough(*(firstPosition + 6) ,  xMax));//x
+    CHECK(closeEnough(*(firstPosition + 7) ,  yMax));//y
+    CHECK(closeEnough(*(firstPosition + 8) ,  0.0f));
+    CHECK(closeEnough(*(firstPosition + 9) ,  xMax));//x
+    CHECK(closeEnough(*(firstPosition + 10),  yMax));//y
+    CHECK(closeEnough(*(firstPosition + 11),  0.0f));
+    CHECK(closeEnough(*(firstPosition + 12),  xMax));//x
+    CHECK(closeEnough(*(firstPosition + 13),  yMin));//y
+    CHECK(closeEnough(*(firstPosition + 14),  0.0f));
+    CHECK(closeEnough(*(firstPosition + 15),  xMin));//x
+    CHECK(closeEnough(*(firstPosition + 16),  yMin));//y
+    CHECK(closeEnough(*(firstPosition + 17),  0.0f));
+}
+
+void checkQuadColors(const float* firstColor, float r, float g, float b)
+{
+    CHECK(closeEnough(*(firstColor + 0) , r));//r
+    CHECK(closeEnough(*(firstColor + 1) , g));//g
+    CHECK(closeEnough(*(firstColor + 2) , b));//b
+    CHECK(closeEnough(*(firstColor + 3) , r));//r
+    CHECK(closeEnough(*(firstColor + 4) , g));//g
+    CHECK(closeEnough(*(firstColor + 5) , b));//b
+    CHECK(closeEnough(*(firstColor + 6) , r));//r
+    CHECK(closeEnough(*(firstColor + 7) , g));//g
+    CHECK(closeEnough(*(firstColor + 8) , b));//b
+    CHECK(closeEnough(*(firstColor + 9) , r));//r
+    CHECK(closeEnough(*(firstColor + 10), g));//g
+    CHECK(closeEnough(*(firstColor + 11), b));//b
+    CHECK(closeEnough(*(firstColor + 12), r));//r
+    CHECK(closeEnough(*(firstColor + 13), g));//g
+    CHECK(closeEnough(*(firstColor + 14), b));//b
+    CHECK(closeEnough(*(firstColor + 15), r));//r
+    CHECK(closeEnough(*(firstColor + 16), g));//g
+    CHECK(closeEnough(*(firstColor + 17), b));//b
+}
+
+void checkQuadTexCoords(const float* firstTexCoord, float xMin, float yMin, float xMax, float yMax)
+{
+    CHECK(closeEnough(*(firstTexCoord + 0) ,  xMin));//u
+    CHECK(closeEnough(*(firstTexCoord + 1) ,  yMin));//v
+    CHECK(closeEnough(*(firstTexCoord + 2) ,  xMin));//u
+    CHECK(closeEnough(*(firstTexCoord + 3) ,  yMax));//v
+    CHECK(closeEnough(*(firstTexCoord + 4) ,  xMax));//u
+    CHECK(closeEnough(*(firstTexCoord + 5) ,  yMax));//v
+    CHECK(closeEnough(*(firstTexCoord + 6) ,  xMax));//u
+    CHECK(closeEnough(*(firstTexCoord + 7) ,  yMax));//v
+    CHECK(closeEnough(*(firstTexCoord + 8) ,  xMax));//u
+    CHECK(closeEnough(*(firstTexCoord + 9) ,  yMin));//v
+    CHECK(closeEnough(*(firstTexCoord + 10),  xMin));//u
+    CHECK(closeEnough(*(firstTexCoord + 11),  yMin));//v
 }
 
 SCENARIO("RenderDataGenerator can be used to turn a gui tree into triangle buffers and such data for rendering", "[logic]")
@@ -70,76 +108,12 @@ SCENARIO("RenderDataGenerator can be used to turn a gui tree into triangle buffe
                 CHECK(data[1].element == root.children()[0].get());
 
                 checkQuadPositions(&data[0].positions[0], 5.0f, 5.0f, 45.0f, 35.0f);
+                checkQuadPositions(&data[1].positions[0], 15.0f, 15.0f, 25.0f, 25.0f);
 
-                CHECK(closeEnough(data[1].positions[0] , 15.0f));//x
-                CHECK(closeEnough(data[1].positions[1] , 15.0f));//y
-                CHECK(closeEnough(data[1].positions[2] ,  0.0f));
-                CHECK(closeEnough(data[1].positions[3] , 15.0f));//x
-                CHECK(closeEnough(data[1].positions[4] , 25.0f));//y
-                CHECK(closeEnough(data[1].positions[5] ,  0.0f));
-                CHECK(closeEnough(data[1].positions[6] , 25.0f));//x
-                CHECK(closeEnough(data[1].positions[7] , 25.0f));//y
-                CHECK(closeEnough(data[1].positions[8] ,  0.0f));
-                CHECK(closeEnough(data[1].positions[9] , 25.0f));//x
-                CHECK(closeEnough(data[1].positions[10], 25.0f));//y
-                CHECK(closeEnough(data[1].positions[11],  0.0f));
-                CHECK(closeEnough(data[1].positions[12], 25.0f));//x
-                CHECK(closeEnough(data[1].positions[13], 15.0f));//y
-                CHECK(closeEnough(data[1].positions[14],  0.0f));
-                CHECK(closeEnough(data[1].positions[15], 15.0f));//x
-                CHECK(closeEnough(data[1].positions[16], 15.0f));//y
-                CHECK(closeEnough(data[1].positions[17],  0.0f));
+                checkQuadColors(&data[0].colors[0], 1.0f, 0.0f, 0.0f);
+                checkQuadColors(&data[1].colors[0], 0.0f, 0.0f, 1.0f);
 
-                CHECK(closeEnough(data[0].colors[0] , 1.0f));//r
-                CHECK(closeEnough(data[0].colors[1] , 0.0f));//g
-                CHECK(closeEnough(data[0].colors[2] , 0.0f));//b
-                CHECK(closeEnough(data[0].colors[3] , 1.0f));//r
-                CHECK(closeEnough(data[0].colors[4] , 0.0f));//g
-                CHECK(closeEnough(data[0].colors[5] , 0.0f));//b
-                CHECK(closeEnough(data[0].colors[6] , 1.0f));//r
-                CHECK(closeEnough(data[0].colors[7] , 0.0f));//g
-                CHECK(closeEnough(data[0].colors[8] , 0.0f));//b
-                CHECK(closeEnough(data[0].colors[9] , 1.0f));//r
-                CHECK(closeEnough(data[0].colors[10], 0.0f));//g
-                CHECK(closeEnough(data[0].colors[11], 0.0f));//b
-                CHECK(closeEnough(data[0].colors[12], 1.0f));//r
-                CHECK(closeEnough(data[0].colors[13], 0.0f));//g
-                CHECK(closeEnough(data[0].colors[14], 0.0f));//b
-                CHECK(closeEnough(data[0].colors[15], 1.0f));//r
-                CHECK(closeEnough(data[0].colors[16], 0.0f));//g
-                CHECK(closeEnough(data[0].colors[17], 0.0f));//b
-
-                CHECK(closeEnough(data[1].colors[0] , 0.0f));//r
-                CHECK(closeEnough(data[1].colors[1] , 0.0f));//g
-                CHECK(closeEnough(data[1].colors[2] , 1.0f));//b
-                CHECK(closeEnough(data[1].colors[3] , 0.0f));//r
-                CHECK(closeEnough(data[1].colors[4] , 0.0f));//g
-                CHECK(closeEnough(data[1].colors[5] , 1.0f));//b
-                CHECK(closeEnough(data[1].colors[6] , 0.0f));//r
-                CHECK(closeEnough(data[1].colors[7] , 0.0f));//g
-                CHECK(closeEnough(data[1].colors[8] , 1.0f));//b
-                CHECK(closeEnough(data[1].colors[9] , 0.0f));//r
-                CHECK(closeEnough(data[1].colors[10], 0.0f));//g
-                CHECK(closeEnough(data[1].colors[11], 1.0f));//b
-                CHECK(closeEnough(data[1].colors[12], 0.0f));//r
-                CHECK(closeEnough(data[1].colors[13], 0.0f));//g
-                CHECK(closeEnough(data[1].colors[14], 1.0f));//b
-                CHECK(closeEnough(data[1].colors[15], 0.0f));//r
-                CHECK(closeEnough(data[1].colors[16], 0.0f));//g
-                CHECK(closeEnough(data[1].colors[17], 1.0f));//b
-
-                CHECK(closeEnough(data[0].texCoords[0] , 0.0f));//u
-                CHECK(closeEnough(data[0].texCoords[1] , 0.0f));//v
-                CHECK(closeEnough(data[0].texCoords[2] , 0.0f));//u
-                CHECK(closeEnough(data[0].texCoords[3] , 0.5f));//v
-                CHECK(closeEnough(data[0].texCoords[4] , 0.5f));//u
-                CHECK(closeEnough(data[0].texCoords[5] , 0.5f));//v
-                CHECK(closeEnough(data[0].texCoords[6] , 0.5f));//u
-                CHECK(closeEnough(data[0].texCoords[7] , 0.5f));//v
-                CHECK(closeEnough(data[0].texCoords[8] , 0.5f));//u
-                CHECK(closeEnough(data[0].texCoords[9] , 0.0f));//v
-                CHECK(closeEnough(data[0].texCoords[10], 0.0f));//u
-                CHECK(closeEnough(data[0].texCoords[11], 0.0f));//v
+                checkQuadTexCoords(&data[0].texCoords[0], 0.0f, 0.0f, 0.5f, 0.5f);
             }
         }
     }
