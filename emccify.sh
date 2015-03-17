e01c37e3b37625f49fcb416af521a2f97878c8c4
Tobias Widlund <widlundtobias@gmail.com>
2015-03-17 18:16:10 +0100

    updated emmcify file

--- emccify.sh
+++ emccify.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cp bin/simple_rendering bin/simple_rendering.bc
-em++ bin/simple_rendering.bc lib/* -o index.html -s USE_SDL=2 -s FULL_ES2=1 -O2 --preload-file resources/ --shell-file shell.html
+em++ bin/simple_rendering.bc lib/* -o web/index.html -s USE_SDL=2 -s FULL_ES2=1 -O2 --preload-file resources/ --shell-file web/shell.html
